fc82aebe8754306e21aad8b79e6ba407e8c4e8fe
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-05-10 21:19:10 -0700

    first draft

---
+++ functions_basic_II.py
@@ -0,0 +1,19 @@
+
+# def countdown():
+#     num = int(input( "provide a number: "))
+    
+#     for i in range (numPlaceholder, -1, -1):      
+#         newList = []
+#         newList.append(i)
+#     return newList
+# countdown( )
+
+# def print_return(list):
+#     print(list[0])
+#     return list[1]
+# print (print_return([2,10]))
+
+def first_plus_len(list):
+    print(list[0])
+    return len(list)
+print(first_plus_len([10,3,2,6,4,4]))
